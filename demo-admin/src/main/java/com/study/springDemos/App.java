4bc97fdfa651e04107e83699ba9b949583f6a712
ZheGui <zhegui2025@163.com>
2025-08-06 19:49:21 +0800

    初始化项目SpringDemos

---
+++ demo-admin/src/main/java/com/study/springDemos/App.java
@@ -0,0 +1,13 @@
+package com.study.springDemos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "ZPK 你好!" );
+    }
+}
